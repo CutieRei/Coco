8d0dbad48bc23e48ecc1da0166604a9fe298d730
ReyterYT <reyteryt5@gmail.com>
2020-08-30 08:34:44 +0700

    Refined the help command and new utility tools

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
-import discord,aiosqlite, datetime
-from discord.ext import commands,tasks
-sql = aiosqlite
+import discord, datetime
+from discord.ext import commands
 bot = commands.Bot(command_prefix=".",case_insensitive=True, activity=discord.Game(name="with the guild members"),help_command=None)
 bot.load_extension('jishaku')
 bot.owner_ids = [477789603403792404,716503311402008577]
+bot.ready_once = False
+bot.uptime = datetime.datetime.utcnow()
 bot.load_extension("category")
 
 
@@ -12,14 +13,7 @@ async def reload(ctx):
     bot.reload_extension("category")
     await ctx.send("Done!")
 
-@tasks.loop(hours=2)
-async def loop():
-    loop.stop()
-    with open('db/data_backup.sql','wb') as backup:
-        with open("db/data.sql",'rb') as db:
-            backup.write(db.read())
 
-loop.start()
 token = None
 with open("token.txt") as file:
     token=file.read()
